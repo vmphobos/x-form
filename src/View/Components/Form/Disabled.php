09d111042b73bd67456dfe35910eb2db59d99d7e
Vasilis Milopoulos <139857686+vmphobos@users.noreply.github.com>
2023-10-27 11:55:43 +0300

    Added Call to Action functionality
    Disabled input fields can now have call to actions and be selectable.
    
    - Adding selectable keyword will make the disabled field selectable and instant copy the relevant text to clipboard.
    - Adding phone, fax, map, mail keywords to <x-form.disabled /> will enable the relevant call to action.

--- src/View/Components/Form/Disabled.php
+++ src/View/Components/Form/Disabled.php
@@ -14,6 +14,11 @@ class Disabled extends Component
         public ?string $floating = null,
         public ?string $icon = null,
         public ?string $tooltip = null,
+        public ?bool $selectable = false,
+        public ?bool $mail = false,
+        public ?bool $phone = false,
+        public ?bool $fax = false,
+        public ?bool $map = false,
     )
     {
         /**
@@ -30,7 +35,30 @@ class Disabled extends Component
                 <x-form.label label="{!! $label !!}" :icon="$icon" />
             @endif
 
-            <div {{ $attributes->class([config('x-form.disabled.class')]) }} style="{{ config('x-form.disabled.style') }}">{{ $value ?: '-' }}</div>
+            <div 
+                {{ 
+                    $attributes->class([
+                        config('x-form.disabled.class'), 
+                        'user-select-all' => $selectable && $value
+                    ]) 
+                }} 
+                style="{{ config('x-form.disabled.style') }}"
+            >
+                {{ $value ?: '-' }}
+                @if($value)
+                    @if($selectable)
+                        <i class="copy-text fa-regular fa-copy float-end mt-1" role="button"></i>
+                    @elseif($mail)
+                        <a href="mailto:{{ $value }}" class="text-dark float-end"><i class="fa-regular fa-envelope"></i></a>
+                    @elseif($phone)
+                        <a href="tel:{{ $value }}" class="text-dark float-end"><i class="fa-solid fa-phone-flip"></i></a>
+                    @elseif($fax)
+                        <a href="fax:{{ $value }}" class="text-dark float-end"><i class="fa-solid fa-fax"></i></a>
+                    @elseif($map)
+                        <a href="http://maps.google.com/?q={{ $value }}" target="_blank"><i class="fa-solid fa-location-dot"></i></a>
+                    @endif
+                @endif
+            </div>
 
             @if($floating && $label)
                     <x-form.label label="{!! $label !!}" :icon="$icon" />
