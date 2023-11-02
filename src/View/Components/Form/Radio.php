27a1f5df7d7100697ca58084d931cb75084a745f
Vasilis Milopoulos <139857686+vmphobos@users.noreply.github.com>
2023-11-02 13:46:39 +0200

    Update Radio.php

--- src/View/Components/Form/Radio.php
+++ src/View/Components/Form/Radio.php
@@ -33,55 +33,6 @@ class Radio extends FormElement
 
     public function render(): View|Closure|string
     {
-        return <<<'HTML'
-            <div wire:key="{{ $uuid }}">
-                @if($label)
-                    <x-form.label for="{{ $uuid }}" label="{!! $label !!}"
-                        :model="$model" :modifier="$modifier" :icon="$icon" :tooltip="$tooltip" :required="$required"
-                    />
-                @endif
-
-                @error($rule)
-                    <div class="{{ config('x-form.error') }}">{{ $message }}</div>
-                @enderror
-
-                <div
-                    @class([
-                        config('x-form.check.vertical') => !$horizontal,
-                        config('x-form.check.horizontal') => $horizontal
-                    ])
-                >
-                    @foreach($list as $title => $id)
-                        <div
-                            @class([
-                                config('x-form.check.div'),
-                                config('x-form.check.inline') => $horizontal
-                            ])
-                        >
-                            <input type="radio" value="{{ $id }}"
-                                {{
-                                    $attributes->class([
-                                        config('x-form.check.input'),
-                                        config('x-form.invalid') => $errors->has($rule)
-                                    ])
-                                    ->merge([
-                                        'id' => str($name)->slug() . '_' . $id,
-                                        'name' => $name,
-                                        'wire:model' . $modifier => $model,
-                                        'wire:key' => str($name)->slug() . '_' . $id,
-                                    ])
-                                }}
-
-                                @if($modifier)
-                                    wire:dirty.class="{{ config('x-form.border') }}"
-                                @endif
-                            >
-
-                            <label class="{{ config('x-form.check.label') }}" for="{{ str($name)->slug() . '_' . $id }}">{{ $title }}</label>
-                        </div>
-                    @endforeach
-                </div>
-            </div>
-        HTML;
+        return view('x-form::radio');
     }
 }
