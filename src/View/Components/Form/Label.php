d9ccb6ebc1e466ad532dbc663dbbef28e8f66ffe
Vasilis Milopoulos <139857686+vmphobos@users.noreply.github.com>
2023-11-02 13:45:19 +0200

    Update Label.php

--- src/View/Components/Form/Label.php
+++ src/View/Components/Form/Label.php
@@ -24,30 +24,6 @@ class Label extends Component
 
     public function render(): View|Closure|string
     {
-        return <<<'HTML'
-            <label for="{{ $for }}"
-                @if($modifier && $model) wire:target="{{ $model }}" @endif
-
-                @class([
-                    config('x-form.label'),
-                    config('x-form.required') => $required
-                ])
-                @if($tooltip)
-                    data-bs-toggle="tooltip"title="{{ $tooltip }}"
-                @endif
-            >
-                @if($icon)
-                    <i class="{{ $icon }}"
-                       @if($modifier && $model) wire:loading.remove wire:target="{{ $model }}" @endif
-                    ></i>
-                @endif
-
-                @if($modifier && $model)
-                    <span wire:loading wire:target="{{ $model }}" class="{{ config('x-form.spinner') }}"></span>
-                @endif
-
-                {!! $label !!}
-            </label>
-        HTML;
+        return view('x-form::label');
     }
 }
