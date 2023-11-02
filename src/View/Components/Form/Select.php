cc945cecb3ba5968d20b3a4b46cded4525638c30
Vasilis Milopoulos <139857686+vmphobos@users.noreply.github.com>
2023-11-02 13:32:48 +0200

    Added Blade

--- src/View/Components/Form/Select.php
+++ src/View/Components/Form/Select.php
@@ -34,57 +34,6 @@ class Select extends FormElement
 
     public function render(): View|Closure|string
     {
-        return <<<'HTML'
-            <div wire:key="{{ $uuid }}">
-                @if($floating)
-                    <div class="{{ config('x-form.floating') }}">
-                @endif
-
-                @if(!$floating && $label)
-                    <x-form.label for="{{ $uuid }}" label="{!! $label !!}"
-                        :model="$model" :modifier="$modifier" :icon="$icon" :tooltip="$tooltip" :required="$required"
-                    />
-                @endif
-
-                <select
-                    {{
-                        $attributes->class([
-                                config('x-form.select'),
-                                config('x-form.invalid') => $errors->has($rule)
-                            ])
-                            ->merge([
-                                'id' => $uuid,
-                                'name' => $name,
-                                'wire:model' . $modifier => $model,
-                                'wire:key' => str($name)->slug(),
-                            ])
-                    }}
-
-                    @if($modifier)
-                        wire:dirty.class="{{ config('x-form.border') }}"
-                    @endif
-
-                    @if($tooltip && !$label)
-                        data-bs-toggle="tooltip" title={{ $tooltip }}
-                    @endif
-                >
-                    <option>{{ $attributes->get('placeholder') }}</option>
-
-                    @foreach($list as $title => $id)
-                        <option value="{{ $id }}" class="text-capitalize">{{ $title }}</option>
-                    @endforeach
-                </select>
-
-                @if($floating && $label)
-                    <x-form.label for="{{ $uuid }}" label="{!! $label !!}"
-                        :model="$model" :modifier="$modifier" :icon="$icon" :tooltip="$tooltip" :required="$required"
-                    />
-                @endif
-
-                @error($rule)
-                    <div class="{{ config('x-form.error') }}">{{ $message }}</div>
-                @enderror
-            </div>
-        HTML;
+         return view('x-form::select');
     }
 }
