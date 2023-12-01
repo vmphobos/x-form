85ba4aa5a5b777b46114025413dc30c01e17ff08
Vasilis Milopoulos <139857686+vmphobos@users.noreply.github.com>
2023-12-01 08:41:13 +0200

    Form Loader

---
+++ views/loader.blade.php
@@ -0,0 +1,9 @@
+<div class="d-none d-flex justify-content-center position-absolute w-100 h-100"
+    wire:target="save" wire:loading.class="bg-light opacity-50" wire:loading.class.remove="d-none"
+>
+    <div class="align-self-center">
+        <div class="spinner-border" role="status">
+            <span class="visually-hidden">Loading...</span>
+        </div>
+    </div>
+</div>
