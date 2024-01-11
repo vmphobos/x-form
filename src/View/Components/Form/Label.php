6c12f976d91cddc3069fda7f4ee3a71e632c1966
Vasilis Milopoulos <139857686+vmphobos@users.noreply.github.com>
2024-01-11 10:54:55 +0200

    Update Label.php

--- src/View/Components/Form/Label.php
+++ src/View/Components/Form/Label.php
@@ -8,18 +8,17 @@ use Illuminate\View\View;
 
 class Label extends Component
 {
-
     public function __construct(
-        public ?string $for = '',
-        public ?string $label = '',
-        public ?string $model = '',
-        public ?string $modifier = '',
-        public ?string $icon = '',
-        public ?string $tooltip = '',
+        public ?string $for = null,
+        public ?string $label = null,
+        public ?string $model = null,
+        public ?string $modifier = null,
+        public ?string $icon = null,
+        public ?string $tooltip = null,
         public ?bool $required = false
     )
     {
-        //...
+        // FORM LABEL
     }
 
     public function render(): View|Closure|string
