478aa7abc430dec73362dca631ceee203cfdee91
Vasilis Milopoulos <139857686+vmphobos@users.noreply.github.com>
2025-11-26 12:14:00 +0200

    Update label.blade.php

--- resources/views/components/form/label.blade.php
+++ resources/views/components/form/label.blade.php
@@ -37,9 +37,8 @@
             <path d="M21 12a9 9 0 1 1-6.219-8.56" />
         </svg>
     @endif
-
-    {{-- Label title --}}
-    <span class="capitalize">{!! $label !!}</span>
+    
+    {!! $label !!}
 </label>
 
 {{-- Tooltip icon --}}
